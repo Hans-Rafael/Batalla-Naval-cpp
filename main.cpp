fd014eee235d5187ebeedcf279e563c871c78033
Your Name <your.email@example.com>
2026-07-08 19:24:20 -0300

    Feat: main creada

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include "juego.h"
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    limpiarConsola();
+    cout << "\n ~~Batalla Naval~~\n";
+    cout << "~~~~~~~~~~~~~~~~~~~\n";
+
+    menu();
+
+    return 0;
+}
